108bb29ffa907ea8a0ad3528852ae301b64e57b0
Marco Galardini <marco.galardini@twincore.de>
2022-06-23 15:49:19 +0200

    Change first steps (2)

--- src/public_seq_filter.py
+++ src/public_seq_filter.py
@@ -28,11 +28,12 @@ if __name__ == '__main__':
 
     # Load tsv with sequences
     sequences = open(args.public)
-    keep = [l.rstrip() for l in sequences.readlines()]
+    keep = {l.rstrip() for l in sequences.readlines()}
 
     # Load high quality seqs selected for tree in GISAID
     gisaid = open(args.metadata)
-    gisaid_tree = {l.rstrip().split(',')[0] for l in gisaid}
+    gisaid_tree = {l.rstrip().split(',')[0] for l in gisaid
+                   if l.rstrip().split(',')[0] in keep}
 
     if arg.n is not None:
         selected = random.choices(gisaid_tree, k=args.n)
