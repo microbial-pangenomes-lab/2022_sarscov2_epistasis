49ab69cb484cd46bf86ee6c4e1548fd34c750b52
Marco Galardini <marco.galardini@twincore.de>
2023-04-26 16:44:16 +0200

    Missing script

---
+++ src/get_end_time.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+
+import os
+import sys
+import argparse
+
+
+def get_options():
+    description = 'Get exact date for the time weight thing'
+    parser = argparse.ArgumentParser(description=description)
+
+    parser.add_argument('yearmonth')
+
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    args = get_options()
+
+    year, month = args.yearmonth.split('-')
+    month = int(month)
+
+    # 30 di conta novembre con april giugno e settembre
+    if month in (11, 4, 6, 9):
+        day = 30
+    # di 28 ce n'e' uno
+    elif month == 2:
+        day = 28
+    # tuti gli altri ne han 31
+    else:
+        day = 31
+    print(f'{year} {month} {day}')
