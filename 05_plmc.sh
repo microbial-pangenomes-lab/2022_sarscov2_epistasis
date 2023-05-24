2208f5cd21fc3de1572f65cdb22df62e1c662f16
Marco Galardini <marco.galardini@twincore.de>
2023-05-24 10:18:43 +0200

    Speed up plmc

--- 05_plmc.sh
+++ 05_plmc.sh
@@ -13,4 +13,4 @@ name=$(basename $fasta .fasta.xz)
 
 xzcat $fasta | python3 src/subset_alignment.py --start 21562 --end 25384 > data/spikes/$name.fasta
 
-plmc/bin/plmc -c $odir/$name.EC -o $odir/$name.params -le 20.0 -lh 0.01 -a -AGCT -n $cores -f EPI_ISL_402130 data/spikes/$name.fasta
+plmc/bin/plmc -c $odir/$name.EC -o $odir/$name.params --fast -m 20 -le 20.0 -lh 0.01 -a -AGCT -n $cores -f EPI_ISL_402130 data/spikes/$name.fasta
