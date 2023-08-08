f4875dbf47d646922795550a03713dc463c77729
Marco Galardini <marco.galardini@twincore.de>
2023-08-08 07:54:46 +0200

    Add end-time to second bash script

--- 02_split.sh
+++ 02_split.sh
@@ -3,13 +3,15 @@ set -eo pipefail
 
 cores=$1
 metadata=$2
+# in the format YYYY-MM
+time=$3
 
 mkdir -p data/subsets
 xzcat prefiltered.fasta.xz | python3 src/split_randomly.py data/tree_subset.txt data/subsets 1000 10000 100000 1000000
 xz -T $cores data/subsets/*.fasta
 
 mkdir -p data/time
-xzcat prefiltered.fasta.xz | python3 src/split_by_time.py data/tree_subset.txt data/time/
+xzcat prefiltered.fasta.xz | python3 src/split_by_time.py data/tree_subset.txt data/time/ --last $time
 xz -T $cores data/time/*.fasta
 
 nextclade dataset get -n sars-cov-2 -o data/nextclade
