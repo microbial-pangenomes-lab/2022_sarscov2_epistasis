f9241da6f5cb75e947d2142cbe8ca18d8d4602ef
Marco Galardini <marco.galardini@twincore.de>
2023-11-06 08:12:32 +0100

    fix plmc for 10k sequences

--- 05_plmc_10000.sh
+++ 05_plmc_10000.sh
@@ -14,4 +14,5 @@ name=$(basename $fasta .fasta.xz)
 xzcat $fasta | python3 src/subset_alignment.py --start 21562 --end 25384 > data/spikes-10000/$name.fasta
 
 plmc/bin/plmc -c $odir/$name.EC -o $odir/$name.params --fast -m 20 -le 20.0 -lh 0.01 -a -AGCT -n $cores -f EPI_ISL_402130 data/spikes-10000/$name.fasta
-python3 src/annotate_plmc.py data/GCF_009858895.2_ASM985889v3_genomic.gff.gz $odir/$name.EC $odir/plmc.tsv.gz
+python3 src/annotate_plmc.py data/GCF_009858895.2_ASM985889v3_genomic.gff.gz $odir/$name.EC $odir/plmc_prefilter.tsv.gz
+python3 src/plmc_filter.py $odir/plmc_prefilter.tsv.gz $odir/plmc.tsv.gz
