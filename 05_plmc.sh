85e32d13a57defa22d1d657a6aeb0f0b4b3afc16
Marco Galardini <marco.galardini@twincore.de>
2024-07-10 09:55:54 +0200

    Forgot to commit a gzip command

--- 05_plmc.sh
+++ 05_plmc.sh
@@ -17,5 +17,6 @@ mkdir -p $odir/$name
 xzcat $fasta | python3 src/subset_alignment.py --start 21562 --end 25384 > data/$idir/$name.fasta
 
 plmc/bin/plmc -c $odir/$name.EC -o $odir/$name.params --fast -m 20 -le 20.0 -lh 0.01 -a -AGCT -n $cores -f EPI_ISL_402130 data/$idir/$name.fasta
+gzip $odir/$name.EC
 python3 src/annotate_plmc.py data/GCF_009858895.2_ASM985889v3_genomic.gff.gz $odir/$name.EC.gz $odir/$name/plmc_prefilter.tsv.gz --top-scoring 10000
 python3 src/plmc_filter.py $odir/$name/plmc_prefilter.tsv.gz $odir/$name/plmc.tsv.gz
